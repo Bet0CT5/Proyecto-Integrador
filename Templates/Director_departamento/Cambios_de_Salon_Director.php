2e79628ae86b571cead8a695b34c3edba36c3a03
Bet0CT5 <bet0.cavazos@outlook.com>
2023-05-23 16:53:58 -0600

    Reportes de cambios de salon para el director de departamento

--- Templates/Director_departamento/Cambios_de_Salon_Director.php
+++ Templates/Director_departamento/Cambios_de_Salon_Director.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cambios de Salon</title>
+    <title>Cambios de Salones</title>
     <link rel="stylesheet" href="../../Styles/CSS/bootstrap.min.css">
     <link rel="stylesheet" href="../../Styles/CSS/Styles.css">
 </head>
@@ -49,16 +49,43 @@
                <tr class="table-dark">
                  <th scope="col">No de Caso</th>
                  <th scope="col">Fecha</th>
-                 <th scope="col">Matr&iacute;cula</th>
+                 <th scope="col">Matrícula</th>
                  <th scope="col">Nombre</th>
                  <th scope="col">Grupo</th>
                  <th scope="col">Materia</th>
-                 <th scope="col">Salon Actual</th>
-                 <th scope="col">Salon Nuevo</th>
+                 <th scope="col">Salón Actual</th>
+                 <th scope="col">Salón Nuevo</th>
                </tr>
              </thead>
              <tbody>
-                
+             <?php
+require_once '../../phps/conexionBD.php';
+
+// Realizar la consulta en la base de datos
+$consulta = "SELECT r.id_reposicion, f.crn_grupo, m.nombre_materia, m.id_maestro, ma.nombre_maestro, f.fecha AS fecha_falta, r.fecha AS fecha_reposicion, r.numero_salon
+             FROM reposicion r
+             INNER JOIN falta_justificada f ON r.id_falta = f.id_falta
+             INNER JOIN maestro ma ON f.id_maestro = ma.id_maestro
+             INNER JOIN materia m ON f.crn_grupo = m.crn_grupo
+             WHERE f.id_maestro = :id_maestro";
+$stmt = $conn->prepare($consulta);
+$stmt->bindParam(':id_maestro', $id);
+$stmt->execute();
+
+while ($fila = $stmt->fetch(PDO::FETCH_ASSOC)) {
+  echo "<tr>";
+  echo "<td>" . $fila['id_reposicion'] . "</td>";
+  echo "<td>" . $fila['fecha_falta'] . "</td>";
+  echo "<td>" . $fila['matricula'] . "</td>";
+  echo "<td>" . $fila['nombre'] . "</td>";
+  echo "<td>" . $fila['grupo'] . "</td>";
+  echo "<td>" . $fila['nombre_materia'] . "</td>";
+  echo "<td>" . $fila['salon_actual'] . "</td>";
+  echo "<td>" . $fila['salon_nuevo'] . "</td>";
+  echo "</tr>";
+}
+
+?>
              </tbody>
            </table>
        </div>
